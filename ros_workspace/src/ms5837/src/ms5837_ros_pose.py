1174625c967fd6cf8673e0c2ad4e3e1b07bc2f2b
ajgrinds <ajgrinds03@gmail.com>
2019-02-23 12:01:30 -0800

    Added variance

--- ros_workspace/src/ms5837/src/ms5837_ros_pose.py
+++ ros_workspace/src/ms5837/src/ms5837_ros_pose.py
@@ -15,7 +15,7 @@ header.frame_id = 'ms5837_pose_data'
 header.stamp = rospy.Time.now()
 ros_msg.header = header
 ros_msg.pose.pose.position.z = 0
-ros_msg.pose.covariance[9] =
+ros_msg.pose.covariance[8] = 0.0001349092722
 
 
 def callback(msg):

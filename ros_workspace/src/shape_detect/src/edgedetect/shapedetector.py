5aa4d0c42a3c9deade46e32a140471b9a966e02e
ajgrinds <ajgrinds03@gmail.com>
2019-03-30 10:47:52 -0700

    Fixed crack finder

---
+++ ros_workspace/src/shape_detect/src/edgedetect/shapedetector.py
@@ -0,0 +1,77 @@
+# import the necessary packages
+import cv2
+import numpy as np
+
+
+class ShapeDetector:
+	def __init__(self):
+		self.s1 = self.s2 = self.s3 = self.s4 = self.s5 = 0
+		pass
+
+	def detect(self, c):
+		# initialize the shape name and approximate the contour
+		shape = "unidentified"
+		peri = cv2.arcLength(c, True)
+		approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+
+		# if the shape is a triangle, it will have 3 vertices
+		# if len(approx) == 2:
+		# shape = "line"
+		# s1 = s1 + 1
+		if len(approx) == 3:
+			shape = "triangle"
+			self.s2 = self.s2 + 1
+
+		# if the shape has 4 vertices, it is either a square or
+		# a rectangle
+		elif len(approx) == 4:
+			# compute the bounding box of the contour and use the
+			# bounding box to compute the aspect ratio
+			(x, y, w, h) = cv2.boundingRect(approx)
+			ar = w / float(h)
+
+			# a square will have an aspect ratio that is approximately
+			# equal to one, otherwise, the shape is a rectangle
+			if 0.75 <= ar <= 1.35:
+				shape = "square"
+				self.s3 = self.s3 + 1
+			else:
+				shape = "rectangle"
+				self.s4 = self.s4 + 1
+
+		# if the shape is a pentagon, it will have 5 vertices
+		# elif len(approx) == 5:
+			# shape = "pentagon"
+
+		# otherwise, we assume the shape is a circle
+		else:
+			shape = "circle"
+			self.s5 += 1
+
+		# return the name of the shape
+		print(self.s1, "lines,", self.s2, "triangles,", self.s3, "Squares,", self.s4, "rectangles,", self.s5, "circles")
+		c1 = str(self.s1)
+		c2 = str(self.s2)
+		c3 = str(self.s3)
+		c4 = str(self.s4)
+		c5 = str(self.s5)
+		blackdrop = np.zeros((1024, 512, 3), np.uint8)
+		font = cv2.FONT_HERSHEY_SIMPLEX
+		cv2.putText(blackdrop,c2,(10,120), font, 4,(255,255,255),2,cv2.LINE_AA)
+		cv2.putText(blackdrop,c3,(10,230), font, 4,(255,255,255),2,cv2.LINE_AA)
+		p1 = (155, 40)
+		p2 = (120, 120)
+		p3 = (170, 120)
+		# Drawing the triangle with the help of lines
+		#  on the black window With given points
+		# cv2.line is the inbuilt function in opencv library
+		cv2.line(blackdrop, p1, p2, (255, 0, 0), 3)
+		cv2.line(blackdrop, p2, p3, (255, 0, 0), 3)
+		cv2.line(blackdrop, p1, p3, (255, 0, 0), 3)
+		cv2.rectangle(blackdrop,(125,155),(195,225),(0,255,0),-1)
+		cv2.putText(blackdrop,c4,(10,340), font, 4,(255,255,255),2,cv2.LINE_AA)
+		cv2.rectangle(blackdrop,(155,265),(165,335),(255,255,0),-1)
+		cv2.putText(blackdrop,c5,(10,450), font, 4,(255,255,255),2,cv2.LINE_AA)
+		cv2.circle(blackdrop,(160,410), 40, (0,0,255), -1)
+		cv2.imshow("blackdrop", blackdrop)
+		return shape
---
+++ ros_workspace/src/shape_detect/src/shape_detect.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env/
+# USAGE
+# python detect_shapes.py --image shapes_and_colors.png
+
+
+# import the necessary packages
+from edgedetect.shapedetector import ShapeDetector
+import imutils
+import cv2
+import numpy as np
+import rospy
+
+
+class ShapeDetect:
+    def __init__(self):
+        self.image_sub = rospy.Subscriber("/image_raw", Image, self.camera_callback)
+
+
+    def camera_callback(self, data):
+        # load the image/video
+        cap = cv2.VideoCapture('video.mp4')
+        while True:
+    	       # Capture frame-by-frame
+    	         ret, frame = cap.read()
+    	         cv2.rectangle(frame, (110, 400), (510, 100), (255, 255, 255), 3)
+    	         cv2.imshow('frame', frame)
+        	if cv2.waitKey(1) & 0xFF == ord('p'):
+        		break
+        # crop image to frame and resize it for better processing!
+        y = 110
+        x = 120
+        h = 285
+        w = 380
+        crop = frame[y:y+h, x:x+w]
+        resized = imutils.resize(crop, width=520)
+        ratio = crop.shape[0] / float(resized.shape[0])
+
+        # convert the resized image to grayscale, blur it slightly,
+        # and threshold it
+        gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+        lower = np.array([0, 0, 0])
+        upper = np.array([60, 60, 60])
+        ret, thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY_INV)
+        cv2.imshow("thresh", thresh)
+        # find contours in the thresholded image and initialize the
+        # shape detector
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+        sd = ShapeDetector()
+
+        # loop over the contours
+        for c in cnts:
+        	# compute the center of the contour, then detect the name of the
+        	# shape using only the contour
+        	M = cv2.moments(c)
+        	cX = int((M["m10"] / (M["m00"] + 1e-7)) * ratio)
+        	cY = int((M["m01"] / (M["m00"] + 1e-7)) * ratio)
+        	shape = sd.detect(c)
+
+        	# multiply the contour (x, y)-coordinates by the resize ratio,
+        	# then draw the contours and the name of the shape on the image
+        	c = c.astype("float")
+        	c *= ratio
+        	c = c.astype("int")
+        	cv2.drawContours(crop, [c], -1, (0, 255, 0), 2)
+        	cv2.putText(crop, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (100, 100, 100), 2)
+        	# show the output image
+        cv2.imshow("Image", crop)
+
+        cv2.waitKey(0)
+
+def main():
+    while not rospy.is_shutdown():
+        rospy.init_node('line_follower')
+        crack_finder = ShapeDetect()
+        rospy.spin()
+
+if __name__ == "__main__":
+    main()

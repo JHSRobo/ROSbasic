70a8b1c84cde308ab5ec46af6982f302e0ebd803
ajgrinds <ajgrinds03@gmail.com>
2019-04-27 10:11:50 -0700

    commented out the image drawing code

--- ros_workspace/src/shape_detection/src/shape_detect.py
+++ ros_workspace/src/shape_detection/src/shape_detect.py
@@ -78,39 +78,41 @@ def find_the_shape(data):
             # circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
             # if circles is not None:
             # s5 += 1
-
+        """
+        THE DRAWING OF THE IMAGE FOR OUTPUT YOU CAN USE THIS LATER IF YOU WOULD LIKE
+        """
         # multiply the contour (x, y)-coordinates by the resize ratio,
         # then draw the contours and the name of the shape on the image
-        c = c.astype("float")
-        c *= ratio
-        c = c.astype("int")
-        cv2.drawContours(crop, [c], -1, (0, 255, 0), 2)
-        cv2.putText(crop, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (100, 100, 100), 2)
+        # c = c.astype("float")
+        # c *= ratio
+        # c = c.astype("int")
+        # cv2.drawContours(crop, [c], -1, (0, 255, 0), 2)
+        # cv2.putText(crop, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (100, 100, 100), 2)
         # converts ints to strings
-        c2 = str(s2)
-        c3 = str(s3)
-        c4 = str(s4)
-        c5 = str(s5)
+        # c2 = str(s2)
+        # c3 = str(s3)
+        # c4 = str(s4)
+        # c5 = str(s5)
         # creates blackdrop for GUI
-        blackdrop = np.zeros((512, 512, 3), np.uint8)
-        font = cv2.FONT_HERSHEY_SIMPLEX
+        # blackdrop = np.zeros((512, 512, 3), np.uint8)
+        # font = cv2.FONT_HERSHEY_SIMPLEX
         # displays number of triangles
-        cv2.putText(blackdrop,c2,(10,120), font, 4,(255,255,255),2,cv2.LINE_AA)
+        # cv2.putText(blackdrop,c2,(10,120), font, 4,(255,255,255),2,cv2.LINE_AA)
         # displays number of Squares
-        cv2.putText(blackdrop, c3, (10, 230), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
-        p1 = (166, 40)
-        p2 = (130, 120)
-        p3 = (200, 120)
+        # cv2.putText(blackdrop, c3, (10, 230), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+        # p1 = (166, 40)
+        # p2 = (130, 120)
+        # p3 = (200, 120)
         # Drawing the triangle with the help of lines
-        cv2.line(blackdrop, p1, p2, (0, 0, 255), 3)
-        cv2.line(blackdrop, p2, p3, (0, 0, 255), 3)
-        cv2.line(blackdrop, p1, p3, (0, 0, 255), 3)
-        cv2.rectangle(blackdrop, (125, 155), (195, 225), (0, 0, 255), -1)
-        cv2.putText(blackdrop, c4, (10, 340), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
-        cv2.rectangle(blackdrop, (155, 265), (165, 335), (0, 0, 255), -1)
-        cv2.putText(blackdrop, c5, (10, 450), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
-        cv2.circle(blackdrop, (160, 410), 40, (0, 0, 255), -1)
-        cv2.imshow("blackdrop", blackdrop)
+        # cv2.line(blackdrop, p1, p2, (0, 0, 255), 3)
+        # cv2.line(blackdrop, p2, p3, (0, 0, 255), 3)
+        # cv2.line(blackdrop, p1, p3, (0, 0, 255), 3)
+        # cv2.rectangle(blackdrop, (125, 155), (195, 225), (0, 0, 255), -1)
+        # cv2.putText(blackdrop, c4, (10, 340), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+        # cv2.rectangle(blackdrop, (155, 265), (165, 335), (0, 0, 255), -1)
+        # cv2.putText(blackdrop, c5, (10, 450), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+        # cv2.circle(blackdrop, (160, 410), 40, (0, 0, 255), -1)
+        # cv2.imshow("blackdrop", blackdrop)
 
         # show the output image
     msg = shape_detect()
@@ -130,8 +132,8 @@ def find_the_shape(data):
     pub = rospy.Publisher("/rov/shape_output", shape_detect, queue_size=3)
     pub.publish(msg)
 
-    cv2.imshow("Image", crop)
-    cv2.waitKey(0)
+    # cv2.imshow("Image", crop)
+    # cv2.waitKey(0)
     exit(0)
 
 
